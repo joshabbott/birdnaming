13eb83c5c1573bae4a7e31467a44d15dbc7d0083
Charles Kemp <c.kemp@unimelb.edu.au>
2020-02-02 22:22:00 +1100

    added morphospace to paper

--- cogsci2020/code/data/add_morphospace.py
+++ cogsci2020/code/data/add_morphospace.py
@@ -1,6 +1,6 @@
 # makedf_zapotec.py
 #
-# creates pandas df with columns:
+# adds density score and 2d embedding (e1, e2) to the zapotec df, which has
 # ['species','length','mass','freq','folk_generic','folk_specific','clements_group','prototype']
 
 
@@ -20,29 +20,43 @@ ms['Binomial'] = ms['Binomial'].str.replace('_',' ')
 
 # pull in PC coordinates
 alldf = pd.merge(df, ms, how='left', left_on='species', right_on='Binomial')
+
+# keep only those with PC coords that are named
+kddf = alldf[alldf['PC1'].notnull() & alldf['folk_generic'].notnull()]
+
 # keep only those with PC coords that are named and have freq > 0
-kddf = alldf[(alldf['PC1'].notnull()) & (alldf['freq'] > 0) & (alldf['folk_generic'].notnull())]
+# kddf = alldf[(alldf['PC1'].notnull()) & (alldf['freq'] > 0) & (alldf['folk_generic'].notnull())]
 # keep only those with PC coords that have freq > 0
-#kddf = alldf[(alldf['PC1'].notnull()) & (alldf['freq'] > 0)]
+# keep only those with PC coords that have freq > 2 or named and freq > 0
+#kddf = alldf[(alldf['PC1'].notnull()) & ((alldf['freq'] > 2) | ((alldf['freq']>0) & alldf['folk_generic'].notnull()))]
 # pull out PC columns
+
 Xdf = kddf.iloc[:, 12:21]
 X = Xdf.values
 
+# use PC1 and PC2 as embedding
+# X_embedded = X[:, 0:2]
+
+# make tSNE embedding
 X_embedded = TSNE(n_components=2, init="pca").fit_transform(X)
 
+## make density using kde (currently unused)
+#
+#ws = kddf['freq']
+#ws = np.log(ws.values)
+#ws[ws==0] = 0.5 * np.log(2)
+## unweighted for now
+#ws[ws >0] = 1
+## use kernel density estimation to compute neighborhood density
+#kde = KernelDensity(kernel='exponential', bandwidth=bw).fit(X, sample_weight=ws)
+#kdescores = kde.score_samples(X)
 
-ws = kddf['freq']
-ws = np.log(ws.values)
-ws[ws==0] = 0.5 * np.log(2)
-# unweighted for now
-ws[ws >0] = 1
-# use kernel density estimation to compute neighborhood density
-kde = KernelDensity(kernel='exponential', bandwidth=bw).fit(X, sample_weight=ws)
-kdescores = kde.score_samples(X)
+# define density as number of nearby neighbors in embedding
 
 pwdists = squareform(pdist(X_embedded))
 pwdists[pwdists==0] = 100
 nndist = np.min(pwdists, axis=1)
+# when included only those that are named with freq > 0
 nbdist = np.sum(pwdists < 0.5 , axis=1)
 
 
