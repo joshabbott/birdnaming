8334b8d58c2c307b6dad8147f1441841c7a7c3e4
joshabbott <joshua.abbott@unimelb.edu.au>
2020-03-03 22:31:51 +1100

    modularized informativeness analyses

--- code/informativeness-zapotec_scratch.py
+++ code/informativeness-zapotec_scratch.py
@@ -80,11 +80,76 @@ def computeRandomShuffles(bird_list,bird_labels,simMatrix,need_probs):
 		for bird in bird_list:
 			lmap_rand[i][bird] = bird_labels[j]
 			j+=1
-		print(lmap_rand[i])
+		# print(lmap_rand[i])
 		evalue_rand[i] = ERE(lmap_rand[i],simMatrix,need_probs)
-		print("Uniform need probs (RAND): ", evalue_rand[i])
+		# print("Uniform need probs (RAND): ", evalue_rand[i])
 
 	return evalue_rand
+
+
+
+def run_informativeness_on_basic_level(birds):
+
+
+	bird_pcs = {}
+	for bird in birds:
+		bird_features = np.array(pc_data[['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9']][pc_data['Binomial'] == bird.replace(' ','_')])
+		if len(bird_features) > 0:
+			bird_pcs[bird] = bird_features
+
+
+	bird_list = list(bird_pcs.keys())
+
+	simMatrix = createSimMatrix(bird_list,bird_pcs)
+
+	# # need to create lmap['species']['folk_specific']
+	# lmap = {}
+	# for bird in bird_list:
+	# 	lmap[bird] = list(df[df['species'] == bird]['folk_specific'])[0] 
+
+
+	# get bird folk specific labels
+	bird_labels = []
+	for bird in bird_list:
+		bird_labels.append(list(df[df['species'] == bird]['folk_specific'])[0])
+	bird_labels_orig = bird_labels
+
+	# make label mapping dictionary
+	lmap = {}
+	i=0
+	for bird in bird_list:
+		lmap[bird] = bird_labels_orig[i]
+		i+=1
+
+
+	# create need probs with uniform distribution
+	uniform_need_probs = {bird: (1.0/len(bird_list)) for bird in bird_list}
+
+	# compute ERE with uniform need probs
+	evalue = ERE(lmap,simMatrix,uniform_need_probs)
+	print("Uniform need probs (Zapotec birds): ", evalue)
+
+	# create need probs with ebird relative freqs
+	freq_need_probs = {}
+	for bird in bird_list:
+		freq_need_probs[bird] = float(df[df['species'] == bird]['freq'])
+	freq_sum = np.sum(list(freq_need_probs.values()))
+
+	for bird in bird_list:
+		freq_need_probs[bird] = freq_need_probs[bird]/freq_sum
+
+	# compute ERE with freq need probs
+	evalue_freq = ERE(lmap,simMatrix,freq_need_probs)
+	print("Freq need probs (Zapotec birds): ", evalue_freq)
+
+	# RANDOM SHUFFLES WITH UNIFORM NEED
+	# shuffle labels and recompute E
+	evalue_rands_uniform = computeRandomShuffles(bird_list,bird_labels,simMatrix,uniform_need_probs)
+	print("RAND shuffle with uniform need probs: ",evalue_rands_uniform)
+
+	## RANDOM SHUFFLES WITH 
+	evalue_rands_freq = computeRandomShuffles(bird_list,bird_labels,simMatrix,freq_need_probs)
+	print("RAND shuffle with freq need probs: ",evalue_rands_freq)
 
 
 
@@ -96,80 +161,80 @@ pc_data = pd.read_csv('./data/41559_2019_1070_MOESM3_ESM.csv')
 
 
 
+
 # just need to change this for other birds
 # work on just hummingbirds for now
 hummingbirds = list(df[df['folk_generic'] == 'dzǐn̲g']['species'])
 
+# birds = list(df[df['folk_generic'] == 'dzǐn̲g']['species'])
+
+run_informativeness_on_basic_level(hummingbirds)
 
 
+sparrow_birds = list(df[df['folk_generic'] == 'mtsùu']['species'])
+
+run_informativeness_on_basic_level(sparrow_birds)
 
 
+# bird_pcs = {}
+# for bird in birds:
+# 	bird_features = np.array(pc_data[['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9']][pc_data['Binomial'] == bird.replace(' ','_')])
+# 	if len(bird_features) > 0:
+# 		bird_pcs[bird] = bird_features
 
-hummingbird_pcs = {}
-for bird in hummingbirds:
-	bird_features = np.array(pc_data[['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9']][pc_data['Binomial'] == bird.replace(' ','_')])
-	if len(bird_features) > 0:
-		hummingbird_pcs[bird] = bird_features
+
+# bird_list = list(bird_pcs.keys())
+
+# simMatrix = createSimMatrix(bird_list,bird_pcs)
+
+# # # need to create lmap['species']['folk_specific']
+# # lmap = {}
+# # for bird in bird_list:
+# # 	lmap[bird] = list(df[df['species'] == bird]['folk_specific'])[0] 
 
 
-bird_list = list(hummingbird_pcs.keys())
+# # get bird folk specific labels
+# bird_labels = []
+# for bird in bird_list:
+# 	bird_labels.append(list(df[df['species'] == bird]['folk_specific'])[0])
+# bird_labels_orig = bird_labels
 
-simMatrix = createSimMatrix(bird_list,hummingbird_pcs)
+# # make label mapping dictionary
+# lmap = {}
+# i=0
+# for bird in bird_list:
+# 	lmap[bird] = bird_labels_orig[i]
+# 	i+=1
 
-# need to create lmap['species']['folk_specific']
-lmap = {}
-for bird in bird_list:
-	lmap[bird] = list(df[df['species'] == bird]['folk_specific'])[0] 
 
-# create need probs (uniform vs. ebird relative freq)
-uniform_need_probs = {bird: (1.0/len(bird_list)) for bird in bird_list}
+# # create need probs with uniform distribution
+# uniform_need_probs = {bird: (1.0/len(bird_list)) for bird in bird_list}
 
+# # compute ERE with uniform need probs
 # evalue = ERE(lmap,simMatrix,uniform_need_probs)
-# print("Uniform need probs: ", evalue)
-# In [52]: ERE(lmap,simMatrix,uniform_need_probs)                                                                                                                       
-# Out[52]: 3.6418178563638186
+# print("Uniform need probs (Zapotec birds): ", evalue)
 
+# # create need probs with ebird relative freqs
+# freq_need_probs = {}
+# for bird in bird_list:
+# 	freq_need_probs[bird] = float(df[df['species'] == bird]['freq'])
+# freq_sum = np.sum(list(freq_need_probs.values()))
 
+# for bird in bird_list:
+# 	freq_need_probs[bird] = freq_need_probs[bird]/freq_sum
 
+# # compute ERE with freq need probs
+# evalue_freq = ERE(lmap,simMatrix,freq_need_probs)
+# print("Freq need probs (Zapotec birds): ", evalue_freq)
 
-# randomize set of folk_specific labels and compute E
-# first remake lmap
+# # RANDOM SHUFFLES WITH UNIFORM NEED
+# # shuffle labels and recompute E
+# evalue_rands_uniform = computeRandomShuffles(bird_list,bird_labels,simMatrix,uniform_need_probs)
+# print("RAND shuffle with uniform need probs: ",evalue_rands_uniform)
 
-hummingbirds_labels = []
-for bird in bird_list:
-	hummingbirds_labels.append(list(df[df['species'] == bird]['folk_specific'])[0])
-
-hummingbirds_labels_orig = hummingbirds_labels
-
-lmap = {}
-i=0
-for bird in bird_list:
-	lmap[bird] = hummingbirds_labels_orig[i]
-	i+=1
-
-evalue = ERE(lmap,simMatrix,uniform_need_probs)
-print("Uniform need probs (Zapotec hummingbirds): ", evalue)
-# In [66]: evalue = ERE(lmap,simMatrix,uniform_need_probs) 
-#     ...: print("Uniform need probs: ", evalue)                                                                                                                
-# Uniform need probs:  3.64181785636
-
-
-freq_need_probs = {}
-for bird in bird_list:
-	freq_need_probs[bird] = float(df[df['species'] == bird]['freq'])
-
-freq_sum = np.sum(list(freq_need_probs.values()))
-
-for bird in bird_list:
-	freq_need_probs[bird] = freq_need_probs[bird]/freq_sum
-
-evalue_freq = ERE(lmap,simMatrix,freq_need_probs)
-print("Freq need probs (Zapotec hummingbirds): ", evalue_freq)
-
-
-# shuffle labels and recompute E
-evalue_rands = computeRandomShuffles(bird_list,hummingbirds_labels,simMatrix,uniform_need_probs)
-
+# ## RANDOM SHUFFLES WITH 
+# evalue_rands_freq = computeRandomShuffles(bird_list,bird_labels,simMatrix,freq_need_probs)
+# print("RAND shuffle with freq need probs: ",evalue_rands_freq)
 
 # Uniform need probs (Zapotec hummingbirds):  3.64181785636
 # Freq need probs (Zapotec hummingbirds):  3.63785269426
